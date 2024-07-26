f6800f538eaf3edc1bc2b6bb8425b35e64cae57b
Noah Stiltner <nstilt1@lsu.edu>
2024-07-26 15:52:30 -0500

    fixed clipping.rs so that there's no duplicated function definitions

--- Tester/rust/benches/clipping.rs
+++ Tester/rust/benches/clipping.rs
@@ -1,48 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand_chacha::{rand_core::{RngCore, SeedableRng}, ChaCha8Rng};
+extern crate rust;
+use rust::{fast_atan_less_accurate, fast_atan_more_accurate, landon_clip};
 use core::f32;
-
-const PI_OVER_4: f32 = f32::consts::PI / 4.0;
-
-#[inline(always)]
-pub fn fast_atan(x: f32) -> f32 {
-    return (PI_OVER_4 * x) - (x * (x.abs() - 1.0) * (0.2447 + 0.663*x.abs())); 
-}
-
-const A: f32 = 0.0776509570923569;
-const B: f32 = -0.287434475393028;
-const C: f32 = PI_OVER_4 - A - B;
-
-#[inline(always)]
-pub fn fast_atan_alt(x: f32) -> f32 {
-    let squared = x*x;
-    //return x - (squared*x/3f32) + squared*squared*x/5f32 - (squared*squared*squared*x/7f32)
-    return ((A*squared + B) * squared + C) * x;
-}
-
-#[inline(always)]
-fn landon_clip(input: f32, drive: f32) -> f32
-{
-    let input_drive: f32 = input * drive;
-    return 2.0 / 3.14 * input_drive.atan();
-}
-
-#[inline(always)]
-fn fast_atan_less_accurate(input: f32, drive: f32) -> f32
-{
-    let input_drive: f32 = input * drive;
-    return 2.0 / 3.14 * fast_atan(input_drive);
-}
-
-#[inline(always)]
-fn fast_atan_more_accurate(input: f32, drive: f32) -> f32
-{
-    let input_drive: f32 = input * drive;
-    return 2.0 / 3.14 * fast_atan_alt(input_drive);
-}
 
 const SAMPLE_RATE: usize = 44100;
 
+/// pseudorandomly returns -1 or 1
 fn maybe_negative_one(rng: &mut ChaCha8Rng) -> f32 {
     if rng.next_u32() > (i32::MAX as u32) {
         return -1f32
